f814e6066b6dbc1cf03b6f8cf3b985fca8871fcf
Bram Veenboer <bram.veenboer@gmail.com>
2015-12-27 15:27:19 +0100

    Cleanup of test.py

--- test.py
+++ test.py
@@ -10,25 +10,11 @@ import re
 
 logging.basicConfig(level=logging.INFO)
 
-with Flow() as flow:
-    if True or flow.login('email', 'password'):
+with Flow(restart=True) as flow:
+    if flow.login('email', 'password'):
         start = date(2015, 4, 1)
-        # end = date(2015, 5, 1)
         end = date.today()
-        
-        test = date(2015, 4, 2)
-
-        # print(start <= test <= end)
-        # flow.getEventsInRange(start, end)
-        # flow.parseCalenderEvents()
-        flow.processTraining('Rik_Veenboer_2015-11-10_17-21-59')
+        flow.getEventsInRange(start, end)
+        flow.parseCalenderEvents()
 
 sys.exit()
-
-# class Jobs:
-    # def download(self, id):
-        # print(id)
-        # print(x)
-
-# for job in jobs[:]:    
-    # getattr(Jobs, 'download')(None, *job[1])
